ec1fa09651b67266a438c1f14a2f56ec0ebd52f4
lazistar <hsjin1456@gmail.com>
2020-07-25 22:45:02 +0900

    똥피하기 게임 개발
    지금까지 배운 것을 바탕으로 똥피하기 게임을 개발해 보았다.

--- pygame_basic/quiz.py
+++ pygame_basic/quiz.py
@@ -1,4 +1,5 @@
 import pygame
+from random import *
 ##########################################################
 # 기본 초기화 (반드시 해야 하는 것들)
 pygame.init()
@@ -9,13 +10,38 @@ screen_height = 640 # 세로 크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # 화면 타이틀 설정
-pygame.display.set_caption("게임 이름")
+pygame.display.set_caption("Quiz")
 
 # FPS
 clock = pygame.time.Clock()
 ##########################################################
 
 # 1. 사용자 게임 초기화 (배경 화면, 게임 이미지, 좌표, 속도, 폰트 등)
+
+# 배경 불러오기
+background = pygame.image.load("C:/Users/한상진/Desktop/PythonWorkspace/Nado_Game/pygame_basic/background.png")
+# 캐릭터 불러오기
+character = pygame.image.load("C:/Users/한상진/Desktop/PythonWorkspace/Nado_Game/pygame_basic/character.png")
+character_size = character.get_rect().size # 이미지의 크기를 구해옴
+character_width = character_size[0] # 캐릭터의 가로 크기
+character_height = character_size[1] # 캐릭터의 세로 크기
+character_x_pos = (screen_width / 2) - (character_width / 2) # 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로)
+character_y_pos = screen_height - character_height # 화면 세로 크기 가장 아래에 해당하는 곳에 위치 (세로)
+
+# 이동 위치
+to_x = 0
+# 캐릭터 스피드
+character_speed = 10
+
+# 적(똥) 만들기
+enemy = pygame.image.load("C:/Users/한상진/Desktop/PythonWorkspace/Nado_Game/pygame_basic/enemy.png")
+enemy_size = enemy.get_rect().size
+enemy_width = enemy_size[0] # 적의 가로 크기
+enemy_height = enemy_size[1]
+enemy_x_pos = randrange(0 , screen_width - enemy_width) # 가로 위치 랜덤
+enemy_y_pos = 0
+# 적 스피드
+enemy_speed = 10
 
 running = True
 while running:
@@ -25,12 +51,48 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN: # 키가 눌러졌는지 확인
+            if event.key == pygame.K_LEFT: # 캐릭터를 왼쪽으로
+                to_x -= character_speed # 캐릭터 스피드 만큼 이동
+            elif event.key == pygame.K_RIGHT: # 캐릭터를 오른쪽으로
+                to_x += character_speed
         
+        if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+
     # 3. 게임 캐릭터 위치 정의
+    character_x_pos += to_x
+    enemy_y_pos += enemy_speed
+
+    # 캐릭터 경계 설정
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
 
     # 4. 충돌 처리
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    if character_rect.colliderect(enemy_rect):
+        print("충돌했습니다. 게임을 종료합니다.")
+        running = False
+
+    if enemy_y_pos > screen_height:
+        enemy_x_pos = randrange(0 , screen_width-enemy_width) # 가로 위치 랜덤
+        enemy_y_pos = 0
 
     # 5. 화면에 그리기
+    screen.blit(background, (0, 0)) # 배경 그리기
+    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos)) # 적 그리기
 
     pygame.display.update()
 
